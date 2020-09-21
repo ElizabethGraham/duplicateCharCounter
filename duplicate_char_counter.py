a3e77f4564e84b955b67c775cc1b5271a8101ecf
elizabethgraham <elizabeth.graham2000@gmail.com>
2020-09-20 21:13:15 -0500

    A better duplicate char counter
    The program now counts each letter with a duplicate and the amount of times it occurs

--- duplicate_char_counter.py
+++ duplicate_char_counter.py
@@ -1,9 +1,16 @@
 def duplicate_count(text):
-    duplicates = []
-    count = 0
-    user_input = text.lower()
-    for x in user_input:
-        if user_input.count(x) > 1 and x not in duplicates:
-            duplicates.append(x)
-            count += 1
-    return count
+    char_dict = {}  # Dictionary where we store the Chars and their occurrence amount
+    for x in text:
+        if text.count(x) > 1 and x not in char_dict:  # If x occurs more than once and hasn't already been done
+                char_dict.update({x:text.count(x)})  # Add the key and value to char_dict{}
+    return char_dict
+
+def main():
+    user_input = input("Text input: ")
+    for letter, occurrences in duplicate_count(user_input).items():
+        print("Letters: {}, Occurrences: {}".format(letter, occurrences))
+
+
+
+if __name__ == "__main__":
+    main()
