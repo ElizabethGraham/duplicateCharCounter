114cd0a401110f1d770e7a720169f9384e2910f7
ElizabethGraham <elizabeth.graham2000@gmail.com>
2021-11-11 12:15:15 -0600

    Fixed broken code

--- duplicate_char_counter.py
+++ duplicate_char_counter.py
@@ -1,27 +1,24 @@
-def duplicate_count(text):
-<<<<<<< HEAD
-    char_dict = {}  # Dictionary where we store the Chars and their occurrence amount
-    for x in text:
-        if text.count(x) > 1 and x not in char_dict:  # If x occurs more than once and hasn't already been done
-                char_dict.update({x:text.count(x)})  # Add the key and value to char_dict{}
+import sys
+import logging
+
+
+def duplicate_counter(text):
+    char_dict = {}  # Dictionary where we store the characters and their occurrence amount
+    for i in text:
+        if text.count(i) > 1 and i not in char_dict:  # If i occurs more than once and hasn't already been done
+            char_dict.update({i:text.count(i)})  # Add the key and value to char_dict{}
     return char_dict
 
-def main():
-    user_input = input("Text input: ")
-    for letter, occurrences in duplicate_count(user_input).items():
-        print("Letters: {}, Occurrences: {}".format(letter, occurrences))
 
+def main(args):
+    try:
+        for char, occurrences in duplicate_counter(args[0]).items():  # Iterate through arguments's letters and find their respective occurrence amount
+            print("Characters: {}, Occurrences: {}".format(char, occurrences))  # Print it to console, with easy to read formatting
+    except IndexError:
+        print("Please provide a valid string.")
+    except Exception as e:
+        logging.exception(e)  # Log any exceptions
 
 
 if __name__ == "__main__":
-    main()
-=======
-    duplicates = []
-    count = 0
-    user_input = text.lower()
-    for x in user_input:
-        if user_input.count(x) > 1 and x not in duplicates:
-            duplicates.append(x)
-            count += 1
-    return count
->>>>>>> origin/master
+    main(sys.argv[1:])
